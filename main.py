4b51a4e873be786041e9ac2cffeb2f985c695c62
thiagaodt <thiagoeccardoso@outlook.com>
2025-04-08 09:31:35 -0300

    divisão das funções

--- main.py
+++ main.py
@@ -16,7 +16,15 @@ def monitorar_clipboard():
             ultimo_copiado.pop(0)
     app.after(500, monitorar_clipboard)
 
-def gerar_mensagem(tipo):
+def saudacao_mensagem(tecla):
+    saudacao = saudacao_var.get()
+
+    if tecla == 7:  
+        mensagem = f"{saudacao}! Siga Trucks Rastreamento e Monitoramento agradece seu contato!!!\nQual seu nome e como podemos ajudar?"
+        keyboard.write(mensagem)
+    
+
+def evento_mensagem(tipo):
     saudacao = saudacao_var.get()
 
     if len(ultimo_copiado) < 2:
@@ -33,9 +41,6 @@ def gerar_mensagem(tipo):
     elif tipo == 3:
         mensagem = f"""{saudacao}! {placa} Tudo certo por aí?\nQual o destino?"""
     
-    elif tipo == 4:
-        mensagem = f"""{saudacao}! SIGA TRUCKS agradece seu contato!!!\nQual o seu nome e como podemos ajudar?"""
-
     pyperclip.copy(mensagem)
     keyboard.write(mensagem)
     pyperclip.copy('')
@@ -43,23 +48,27 @@ def gerar_mensagem(tipo):
 app = ttk.Window(themename="darkly")
 app.title("SigaText - Macro de Mensagens")
 app.geometry("600x300")
+app.wm_minsize(600,300)
+app.wm_maxsize(600,300)
 
-tk.Label(app, text="Saudação:").pack(pady=5)
+tk.Label(app, text="Saudação:", font=("Aptos", 16,"bold")).pack(pady=5)
 saudacao_var = ttk.Combobox(app, values=["Bom dia", "Boa tarde", "Boa noite"], bootstyle="primary")
 saudacao_var.current(0)
-saudacao_var.pack()
+saudacao_var.pack(pady=5)
 
+tk.Label(app, text="Mensagens:", font=("Aptos", 16,"bold")).pack(pady=5)
 tk.Label(app, text="CTRL+SHIFT+1 = Alimentação desconectada").pack()
 tk.Label(app, text="Exemplo: [saudacao]! [placa] Alimentação desconectada em [local]" \
 "Tudo certo por aí?").pack()
 tk.Label(app, text="CTRL+SHIFT+2 = Sem comunicação").pack()
 tk.Label(app, text="CTRL+SHIFT+3 = Destino").pack()
-tk.Label(app, text="CTRL+SHIFT+4 = Saudação").pack()
+tk.Label(app, text="CTRL+SHIFT+7 = Saudação").pack()
 
-keyboard.add_hotkey('ctrl+shift+1', lambda: gerar_mensagem(1))
-keyboard.add_hotkey('ctrl+shift+2', lambda: gerar_mensagem(2))
-keyboard.add_hotkey('ctrl+shift+3', lambda: gerar_mensagem(3))
-keyboard.add_hotkey('ctrl+shift+4', lambda: gerar_mensagem(4))
+
+keyboard.add_hotkey('ctrl+shift+1', lambda: evento_mensagem(1))
+keyboard.add_hotkey('ctrl+shift+2', lambda: evento_mensagem(2))
+keyboard.add_hotkey('ctrl+shift+3', lambda: evento_mensagem(3))
+keyboard.add_hotkey('ctrl+shift+7', lambda: saudacao_mensagem(7))
 
 monitorar_clipboard()
 
